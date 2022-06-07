460c381c6d04a2d60caf722f0c4cc5d08c2abf5b
Esther Camilla Akinyi <camillaodhiambo8@gmail.com>
2022-06-07 19:19:39 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  function scuberGreetingForFeet(trip){
+    
+    if(trip <= 400){
+      return 'This one is on me!'
 
-function switchOnCharmFromTip(){
+    } else if(trip > 2000 && trip < 2500) {
+      return 'I will gladly take your thirty bucks.'
+
+    } else {
+      return 'No can do.'
+    }
+
+  }
+
+
+
   // Write your code here!
-}
+ function ternaryCheckCity(city){
+    return city === 'NYC' ? "Ok, sounds good." : "No go."  
+  }
+
+ 
+
+
+  // Write your code here!
+  function switchOnCharmFromTip(tip){
+    switch (tip) {
+      case 'generous' :
+        return 'Thank you so much.'
+      case 'not as generous' :
+        return 'Thank you.'
+      default :
+        return 'Bye.'
+    }
+
+  }
